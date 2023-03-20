b408da7505872c7f14cb3d4c30216f27650bc3e5
Fatch Cock <sdruza4@gmail.com>
2023-03-20 22:11:34 +0300

    Added several UI components (Next, Prev, Play, OkBtn) and have deleted some useless files. By sobar

---
+++ reading-app/src/UI/OkBtn.vue
@@ -0,0 +1,30 @@
+<script setup>
+ 
+</script>
+ 
+<template>
+  <button class="ok">
+    <slot>
+
+    </slot>
+  </button>
+</template>
+ 
+<style scoped>
+  .ok {
+    width: 100px;
+    height: 50px;
+    background-color: #14213D;
+    border: 1px solid #FCA311;
+    color: #FCA311;
+    border-radius: 20px;
+    font-size: 30px;
+    font-weight: 400;
+    line-height: 45px;
+    transition: all .3s ease;
+  }
+  .ok:hover {
+    background-color: #FCA311;
+    color: #14213D;
+  }
+</style>
