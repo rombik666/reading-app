4be64bda5936bdf969f1969e2c82efc6e21d06e2
Sobar <sdruza4@gmail.com>
2023-09-21 19:55:06 +0300

    Changed and designed settings form

--- client/src/features/SetForm/SetForm.vue
+++ client/src/features/SetForm/SetForm.vue
@@ -10,6 +10,10 @@
   const color = ref('#ffffff');
   const theme = ref('default');
 
+  const changeTheme = (mode) => {
+    theme.value = mode;
+  }
+
   const font_values = [
     'Noto Sans',
     'Roboto',
@@ -30,6 +34,12 @@
     '80',
     '100'
   ];
+  const theme_values = [
+    'default',
+    'dark',
+    'pink',
+    'light'
+  ]
 
   const toggleFont = () => {
     size_is_active.value = false;
@@ -76,9 +86,19 @@
       </ul>
     </div>
     <label class="color" for="color-form">
-        <h1>Text color</h1>
-        <input v-model="color" type="color" name="color" id="color-form">
-      </label>
+      <h1>Text color</h1>
+      <input v-model="color" type="color" name="color" id="color-form">
+    </label>
+
+    <div class="theme">
+      <ul class="theme-items">
+        <li v-for="value in theme_values" :class="{active: theme == value ? true : false}" @click="changeTheme(value)" :id="value" :key="value"></li>
+      </ul>
+    </div>
+    <div class="action-group">
+      <ActionBtn>Reset</ActionBtn>
+      <ActionBtn>Save</ActionBtn> 
+    </div>
   </div>
 </template>
  
@@ -124,6 +144,8 @@
     }
   }
   .select-content {
+    color: $text;
+    background: $rest;              
     @extend %set-item;
     @extend %middle-text;
   }
@@ -149,8 +171,53 @@
   }
   
   .color  {
+    color: $text;
+    background: $rest;        
     @extend %set-item;
     @extend %middle-text;
+  }
+  .theme {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+  .theme-items {
+    color: $text;
+    background: $rest;        
+    @extend %set-item;
+    @extend %middle-text;
+    width: fit-content;
+    gap: 20px;
+    li {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      border: 2px solid $text;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      transition: border .2s ease-in-out;
+    }
+    li.active {
+      border: 6px solid gold;
+    }
+    #default {
+      background-image: url('./assets/darkblue_theme.svg');
+    }
+    #dark {
+      background-image: url('./assets/dark_theme.svg');
+    }
+    #pink {
+      background-image: url('./assets/pink_theme.svg');
+    }
+    #light {
+      background-image: url('./assets/black&white_theme.svg');
+    }
+  }
+  .action-group {
+    display: flex;
+    gap: 13px;
+
   }
 
 </style>
